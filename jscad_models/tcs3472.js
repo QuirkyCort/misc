0e2be3a39626bffa322cbf338f6e0e5abba33578
Cort <cort@cortscorner.net>
2024-04-14 09:23:51 +0800

    Change default height Added offset option

--- jscad_models/tcs3472.js
+++ jscad_models/tcs3472.js
@@ -8,9 +8,10 @@ const { cylinder, cuboid } = jscad.primitives
 
 const getParameterDefinitions = () => {
   return [
-    { name: 'height', type: 'float', initial: 12, caption: 'Overall height' },
+    { name: 'height', type: 'float', initial: 18, caption: 'Overall height' },
     { name: 'm3', type: 'float', initial: 2.8, step: 0.1, caption: 'Diameter of M3 holes' },
     { name: 'bigger', type: 'checkbox', checked: false, caption: 'Bigger base' },
+    { name: 'offset', type: 'checkbox', checked: false, caption: 'Offset mount point to one side' },
     { name: 'legoInnerDia', type: 'float', initial: 4.8, step: 0.1, caption: 'Lego: Inner diameter of hole' },
     { name: 'legoOuterDia', type: 'float', initial: 6.2, step: 0.1, caption: 'Lego: Outer diameter of hole' },
     { name: 'legoHeight', type: 'float', initial: 0.8, step: 0.1, caption: 'Lego: Height of outer diameter' },
@@ -18,6 +19,7 @@ const getParameterDefinitions = () => {
 }
 
 const MOUNTING_LENGTH = 25.87;
+const MOUNTING_DIAMETER = 6;
 
 const legoHole = (x, y, z, params) => {
   const inner = params.legoInnerDia
@@ -52,29 +54,35 @@ const main = (params) => {
   const height = params.height;
   const m3 = params.m3;
   const bigger = params.bigger;
+  const offset = params.offset;
 
   // Mounting points for sensor
-  solids.push(cuboid({size: [MOUNTING_LENGTH, 6, 8], center: [0, 0, -4]}))
+  let yPos = 0;
+  if (offset) {
+    if (bigger) {
+      yPos = -12 + MOUNTING_DIAMETER / 2
+    } else {
+      yPos = -8 + MOUNTING_DIAMETER /2
+    }
+  }
 
-  solids.push(cylinder({radius: 3, height: height, center: [MOUNTING_LENGTH/2, 0, -height/2+4], segments: 32}))
-  solids.push(cylinder({radius: 3, height: height, center: [-MOUNTING_LENGTH/2, 0, -height/2+4], segments: 32}))
+  solids.push(cuboid({size: [MOUNTING_LENGTH, 6, 8], center: [0, yPos, -4]}))
 
-  holes.push(cylinder({radius: m3/2, height: height, center: [MOUNTING_LENGTH/2, 0, -height/2+4], segments: 32}))
-  holes.push(cylinder({radius: m3/2, height: height, center: [-MOUNTING_LENGTH/2, 0, -height/2+4], segments: 32}))
+  solids.push(cylinder({radius: MOUNTING_DIAMETER/2, height: height, center: [MOUNTING_LENGTH/2, yPos, height/2-8], segments: 32}))
+  solids.push(cylinder({radius: MOUNTING_DIAMETER/2, height: height, center: [-MOUNTING_LENGTH/2, yPos, height/2-8], segments: 32}))
+
+  holes.push(cylinder({radius: m3/2, height: height, center: [MOUNTING_LENGTH/2, yPos, height/2-8], segments: 32}))
+  holes.push(cylinder({radius: m3/2, height: height, center: [-MOUNTING_LENGTH/2, yPos, height/2-8], segments: 32}))
 
   // Base
   if (bigger) {
     solids.push(cuboid({size: [24, 24, 8], center: [0, 0, -4]}))
 
-    holes.push(legoHole(0, 8, -4, params))
-    holes.push(legoHole(-8, 8, -4, params))
-    holes.push(legoHole(8, 8, -4, params))
-
-    holes.push(legoHole(0, -8, -4, params))
-    holes.push(legoHole(-8, -8, -4, params))
-    holes.push(legoHole(8, -8, -4, params))
-
-    holes.push(legoHole(0, 0, -4, params))
+    for (let x=-1; x<=1; x++) {
+      for (let y=-1; y<=1; y++) {
+        holes.push(legoHole(x*8, y*8, -4, params))
+      }
+    }
   } else {
     solids.push(cuboid({size: [16, 16, 8], center: [0, 0, -4]}))
 
@@ -88,5 +96,3 @@ const main = (params) => {
 }
 
 module.exports = { getParameterDefinitions, main }
-
-
