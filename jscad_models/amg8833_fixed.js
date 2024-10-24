da7e7040b52ddc8873573911bfb92c40c05cef13
Cort <cort@cortscorner.net>
2024-10-24 17:22:06 +0800

    Added amg8833_fixed

--- jscad_models/amg8833_fixed.js
+++ jscad_models/amg8833_fixed.js
@@ -1,5 +1,5 @@
 /**
- * Adapter for Sharp Distance Sensors
+ * Adapter for AMG8833 Sensors
  */
 
 const jscad = require('@jscad/modeling')
@@ -7,12 +7,15 @@ const { union, subtract } = require('@jscad/modeling').booleans
 const { rotateX, translate } = require('@jscad/modeling').transforms
 const { cylinder, cuboid } = jscad.primitives
 
+const MOUNTING_HOLES_DISTANCE = 13.2;
+const MARGIN = 3;
+const MOUNT_DEPTH = 7;
+const PROTRUSION_DEPTH = 1;
+
 const getParameterDefinitions = () => {
   return [
     { name: 'height', type: 'float', initial: 15, caption: 'Height of mouting holes from base' },
-    { name: 'width', type: 'float', initial: 6, step: 0.5, caption: 'Width of mount points' },
-    { name: 'm3', type: 'float', initial: 2.8, caption: 'Diameter of M3 holes' },
-    { name: 'size', type: 'float', initial: 37, caption: 'Distance between sensor mounting holes' },
+    { name: 'm2', type: 'float', initial: 1.8, caption: 'Diameter of M2 holes' },
     { name: 'type', type: 'choice', caption: 'Base type', values: ['A1', 'A2', 'B1', 'B2'], captions: ['A1', 'A2', 'B1', 'B2'], initial: 'A1' },
     { name: 'legoInnerDia', type: 'float', initial: 4.8, caption: 'Lego: Inner diameter of hole' },
     { name: 'legoOuterDia', type: 'float', initial: 6.2, caption: 'Lego: Outer diameter of hole' },
@@ -51,24 +54,21 @@ const main = (params) => {
   const holes = [];
 
   const height = params.height;
-  const width = params.width;
-  const m3 = params.m3;
-  const size = params.size;
+  const m2 = params.m2;
   const type = params.type;
 
-  // Mounting points for sensor
-  let mounting_holes_distance = size;
+  const mountWidth = MOUNTING_HOLES_DISTANCE + MARGIN * 2;
+  const mountHeight = height + MARGIN;
+  const holes_offset = MOUNTING_HOLES_DISTANCE / 2;
+  solids.push(cuboid({size: [mountWidth, MOUNT_DEPTH, mountHeight], center: [0, 0, mountHeight/2]}))
 
-  const holes_offset = mounting_holes_distance / 2
-  solids.push(translate([holes_offset, 0, height], rotateX(Math.PI/2, cylinder({radius: width/2, height: 7, segments: 32}))))
-  solids.push(translate([-holes_offset, 0, height], rotateX(Math.PI/2, cylinder({radius: width/2, height: 7, segments: 32}))))
-  solids.push(cuboid({size: [width, 7, height], center: [holes_offset, 0, height/2]}))
-  solids.push(cuboid({size: [width, 7, height], center: [-holes_offset, 0, height/2]}))
-  solids.push(cuboid({size: [mounting_holes_distance, 7, 8], center: [0, 0, 4]}))
+  const protrusionWidth = MOUNTING_HOLES_DISTANCE - m2 * 2;
+  const protrusionHeight = height + m2 * 1 / 2;
+  const protrusionY = -(MOUNT_DEPTH + PROTRUSION_DEPTH) / 2;
+  solids.push(cuboid({size: [protrusionWidth, PROTRUSION_DEPTH, protrusionHeight], center: [0, protrusionY, protrusionHeight/2]}))
 
-  holes.push(translate([holes_offset, 0, height], rotateX(Math.PI/2, cylinder({radius: m3/2, height: 10, segments: 32}))))
-  holes.push(translate([-holes_offset, 0, height], rotateX(Math.PI/2, cylinder({radius: m3/2, height: 10, segments: 32}))))
-
+  holes.push(translate([holes_offset, 0, height], rotateX(Math.PI/2, cylinder({radius: m2/2, height: 10, segments: 32}))))
+  holes.push(translate([-holes_offset, 0, height], rotateX(Math.PI/2, cylinder({radius: m2/2, height: 10, segments: 32}))))
 
   // Base
   if (type == 'A1') {
@@ -83,12 +83,12 @@ const main = (params) => {
 
   if (type == 'A1' || type == 'A2') {
     for (let x=-1; x<=1; x++) {
-      for (let y=0; y<=2; y++) {
+      for (let y=1; y<=2; y++) {
         holes.push(legoHole(x*8, 0.5+y*8, 4, params))
       }
     }
   } else if (type == 'B1' || type == 'B2') {
-    for (let y=0; y<=2; y++) {
+    for (let y=1; y<=2; y++) {
       holes.push(legoHole(-4, 0.5+y*8, 4, params))
       holes.push(legoHole(4, 0.5+y*8, 4, params))
     }
